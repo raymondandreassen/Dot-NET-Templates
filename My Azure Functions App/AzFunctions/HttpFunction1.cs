01c7eb5ec437ae2dee6f51179dc4cd0e2d664c8a
Raymond Andreassen <Raymond.Andreassen@gmail.com>
2024-02-29 00:11:03 +0100

    Az

--- My Azure Functions App/AzFunctions/HttpFunction1.cs
+++ My Azure Functions App/AzFunctions/HttpFunction1.cs
@@ -3,22 +3,47 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace My_Azure_Functions_App.AzFunctions
+namespace My_Azure_Functions_App.AzFunctions;
+
+public class HttpFunction1
 {
-    public class HttpFunction1
+    private readonly ILogger<HttpFunction1> _logger;
+
+    public HttpFunction1(ILogger<HttpFunction1> logger)
     {
-        private readonly ILogger<HttpFunction1> _logger;
+        _logger = logger;
+    }
 
-        public HttpFunction1(ILogger<HttpFunction1> logger)
+    [Function("Function1")]
+    public IActionResult RunFunction1([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function RunFunction1 processed a request.");
+        string[] res = new string[] { "Hello", "World", "Somewhere", "Just data", "Bye" };
+        return new OkObjectResult(res);
+    }
+
+    [Function("Function2")]
+    public async Task<IActionResult> Function2([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
+        // currently it is not automatically binding properties to model with Isolated    ([FromBody], [FromHeader], etc)
+        _logger.LogInformation("C# HTTP trigger function Function2 processed a request.");
+        try
         {
-            _logger = logger;
+            var something = await req.ReadFromJsonAsync<List<string>>();
+            if (something is null) return new BadRequestObjectResult("Invalid input");
+            return new OkObjectResult(something);
         }
-
-        [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        catch (Exception ex)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            return new BadRequestObjectResult("Invalid input");
         }
     }
+
+
+    [Function("Function400")]   public IActionResult RunFunction400([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)      { return new BadRequestObjectResult("Invalid input"); }    // Error 400
+    [Function("Function401")]   public IActionResult RunFunction401([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)      { return new UnauthorizedObjectResult("Unauthorized"); }    // Error 401
+    [Function("Function404")]   public IActionResult RunFunction404([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)      { return new NotFoundObjectResult("Not found"); }           // Error 404 
+    [Function("Function409")]   public IActionResult RunFunction409([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)      { return new ConflictObjectResult("Conflict input"); }      // Error 409 - will produce a Conflict (409) response.
+    [Function("Function420")]   public IActionResult RunFunction420([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)      { return new UnprocessableEntityObjectResult("Conflict input"); }      // Error 420 - Unprocessable
+
 }
--- My Azure Functions App/Program.cs
+++ My Azure Functions App/Program.cs
@@ -1,14 +1,88 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+// Orginalt 
+//
+//var host = new HostBuilder()
+//    .ConfigureFunctionsWebApplication()
+//    .ConfigureServices(services =>
+//    {
+//        services.AddApplicationInsightsTelemetryWorkerService();
+//        services.ConfigureFunctionsApplicationInsights();
+//    })
+//    .Build();
+//host.Run();
+
+// En periode gjorde vi det slik
+//var host = new HostBuilder()
+//    .ConfigureFunctionsWebApplication()
+//    .ConfigureHostConfiguration(c =>
+//    {
+
+//    })
+
+//    .ConfigureAppConfiguration((hostingContext, configuration) =>
+//    {
+//        configuration.AddUserSecrets<Program>();
+//        configuration.AddEnvironmentVariables();
+//        configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+//    })
+
+//    .ConfigureLogging(logger =>
+//    {
+//        logger.AddFilter("Microsoft.EntityFrameworkCore", LogLevel.Warning);
+//    })
+
+//    .ConfigureServices((hostingcontext, services) =>
+//    {
+//        //services.AddDbContext<MyDbContext>(options => { options.UseSqlServer(Environment.GetEnvironmentVariable("FellaDb")); });
+//        services.AddHttpClient("h_client", config =>
+//        {
+//            config.BaseAddress = new Uri(Environment.GetEnvironmentVariable("Setting"));
+//            config.Timeout = new TimeSpan(0, 0, 120);
+//            config.DefaultRequestHeaders.Clear();
+//            config.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+//            var authString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{Environment.GetEnvironmentVariable("Username")}:{Environment.GetEnvironmentVariable("Password")}"));
+//            config.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authString);
+//        });
+
+//    })
+
+//    .Build();
+//host.Run();
+
+var host = new HostBuilder();
+host.ConfigureFunctionsWebApplication();
+
+host.ConfigureHostConfiguration(configBuilder =>
+    {
+    });
+
+host.ConfigureAppConfiguration((hostingContext, configuration) =>
+    {
+        configuration.AddUserSecrets<Program>();
+        configuration.AddEnvironmentVariables();
+        configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    });
+
+host.ConfigureLogging(logger =>
+    {
+        logger.AddFilter("Microsoft.EntityFrameworkCore", LogLevel.Warning);
+    });
+
+//host.ConfigureMetrics( configureMetrics =>
+//    {
+//    });
+
+host.ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-    })
-    .Build();
+    });
 
-host.Run();
+host.Build().Run();
+
